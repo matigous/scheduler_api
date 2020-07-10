8785c0e7fdfe32e65b612cff09724cda5147dcf5
matigous <matheus.vilela.898@gmail.com>
2020-07-10 12:27:51 -0300

    Add Cors and Avatar to Session

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup'; // Biblioteca para validacao na API
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -71,14 +72,24 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url']
+        }
+      ]
+    })
 
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar
     });
   }
 
